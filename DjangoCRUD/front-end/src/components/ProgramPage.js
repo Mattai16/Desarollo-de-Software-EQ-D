9d6b27b6dee17fea8e3055d3b62c05c7c6f8d990
Kevin Osvaldo Panes Landa <91098125+kpanes@users.noreply.github.com>
2023-03-28 22:58:49 -0600

    Update ProgramPage.js

--- DjangoCRUD/front-end/src/components/ProgramPage.js
+++ DjangoCRUD/front-end/src/components/ProgramPage.js
@@ -10,4 +10,17 @@ function ProgramPage() {
   );
 }
 
+/ Creamos un array con las opciones del menú
+var menu = ["Inicio", "Productos", "Servicios", "Contacto"];
+
+// Creamos una función para mostrar el menú en pantalla
+function mostrarMenu() {
+  for (var i = 0; i < menu.length; i++) {
+    console.log((i + 1) + ". " + menu[i]);
+  }
+}
+
+// Llamamos a la función mostrarMenu() para mostrar el menú en pantalla
+mostrarMenu();
+
 export default ProgramPage;
