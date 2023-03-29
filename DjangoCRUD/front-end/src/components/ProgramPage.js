1954150d7205fb7dd1fbac13582d14f76f638267
Kevin Osvaldo Panes Landa <91098125+kpanes@users.noreply.github.com>
2023-03-28 23:11:00 -0600

    Update ProgramPage.js

--- DjangoCRUD/front-end/src/components/ProgramPage.js
+++ DjangoCRUD/front-end/src/components/ProgramPage.js
@@ -7,18 +7,19 @@ function ProgramPage() {
       <p>Esta Pagina Mostrara el Programa</p>
       <button>Programa</button>
     </div>
+    
+    let opcion1 = "Programa 28/03/2023";
+  let opcion2 = "Programa 29/03/2023";
+  let opcion3 = "Programa 30/03/2023";
+
+  let menu = `${opcion1}\n${opcion2}\n${opcion3}`;
+  console.log(menu);
+
+
+  // Llamamos a la función mostrarMenu() para mostrar el menú en pantalla
+  mostrarMenu();
   );
 }
 
-let opcion1 = "Programa 28/03/2023";
-let opcion2 = "Programa 29/03/2023";
-let opcion3 = "Programa 30/03/2023";
-
-let menu = `${opcion1}\n${opcion2}\n${opcion3}`;
-console.log(menu);
-
-
-// Llamamos a la función mostrarMenu() para mostrar el menú en pantalla
-mostrarMenu();
 
 export default ProgramPage;
