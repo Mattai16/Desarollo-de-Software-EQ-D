a8e4a7ab48b4dbceb1111c9138f5ab104d1a42e5
MauricioAburtoL <xlr.2000@hotmail.com>
2023-03-28 20:24:53 -0600

    Reproductor agregado

--- DjangoCRUD/front-end/src/components/HomePage.js
+++ DjangoCRUD/front-end/src/components/HomePage.js
@@ -6,6 +6,12 @@ function HomePage() {
       <h1>Bienvenido a mi sitio web</h1>
       <p>Esta es la página de inicio</p>
       <button>Home</button>
+      <audio controls>
+       <source src="https://stream-42.zeno.fm/dinseg38t5suv?zs=Qx7iMRLeRzSGllcJJv1Trw" type="audio/mpeg"></source>
+      Your browser does not support the audio element.
+      </audio>
+
+
     </div>
   );
 }
