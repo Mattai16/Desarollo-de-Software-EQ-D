bd51704a9fb8a96dbf015ea6c00eb9e135a0f17f
Kevin Osvaldo Panes Landa <91098125+kpanes@users.noreply.github.com>
2023-03-29 08:46:55 -0600

    Update ProgramPage.js

--- DjangoCRUD/front-end/src/components/ProgramPage.js
+++ DjangoCRUD/front-end/src/components/ProgramPage.js
@@ -10,17 +10,4 @@ function ProgramPage() {
    );
 }
 
-// Creamos un objeto con cada una de las opciones del menú
-var menu = {
-  opcion1: "Inicio",
-  opcion2: "Acerca de nosotros",
-  opcion3: "Productos",
-  opcion4: "Contacto"
-};
-
-// Recorremos el objeto para mostrar cada opción del menú
-for (var opcion in menu) {
-  document.write("<a href='#'>" + menu[opcion] + "</a> ");
-}
-
 export default ProgramPage;
